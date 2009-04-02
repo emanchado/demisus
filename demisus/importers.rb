69563ab8793d1777731e9f5f3ab81e7053225a04
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2009-04-02 23:05:06 +0200

    Add importer from_simple_string

--- demisus/importers.rb
+++ demisus/importers.rb
@@ -1,8 +1,12 @@
 module Demisus
   module Importers
     def from_simple_file(path)
+      return from_simple_string(File.read(path))
+    end
+
+    def from_simple_string(string)
       numbers = []
-      File.readlines(path).each do |line|
+      string.each_line do |line|
         row = []
         line.chomp.each_char do |c|
           row << (c == '_' ? nil : c.to_i)
@@ -13,5 +17,6 @@ module Demisus
     end
 
     module_function :from_simple_file
+    module_function :from_simple_string
   end
 end
